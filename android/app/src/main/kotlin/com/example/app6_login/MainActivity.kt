345f7ef2b584cfbc49990404cd0266c438988e94
Yonil <201913301c@utea.edu.pe>
2022-10-18 20:17:12 -0500

    init login

---
+++ android/app/src/main/kotlin/com/example/app6_login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app6_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent,
+        body:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Container(
+            padding: EdgeInsets.only(left: 15.0,right: 15.0,top:190.0,bottom: 190.0 ),
+            margin: EdgeInsets.all(10.0),
+            color: Color.fromARGB(255, 95, 209, 238),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 60.0,
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage("https://images.pexels.com/photos/5558237/pexels-photo-5558237.jpeg"),
+                ),
+                SizedBox(height: 50.0,),
+
+                Card(
+                  child: ListTile(
+                    title: Text("Email address"),
+                    subtitle: Container(
+                      child: Row(
+                        children: [
+                        Icon(Icons.mail_outline_outlined,color: Colors.black,),
+                        Container(
+                          padding: EdgeInsets.only(left: 5.0),
+                          child: Text("Useranme@gmail.com",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                          ),),
+                        )
+                      ],),
+                    ),
+                  ),
+                ),
+              SizedBox(height: 15.0,),
+                Card(
+                  child: ListTile(
+                    title: Text("Password"),
+                    subtitle: Container(
+                      child: Row(children: [
+                        Icon(Icons.lock_outline_rounded,
+                        color: Colors.black),
+                        Container(
+                          padding: EdgeInsets.only(left: 5.0,right: 180.0),
+                          child: Text("*************",
+                          style: TextStyle(color: Colors.black,
+                          fontWeight: FontWeight.bold),),
+                        ),
+                        Icon(Icons.remove_red_eye_outlined,
+                        color: Colors.black),
+                      ],),
+                    ),
+                      
+                      ),
+                  ),
+              
+              ElevatedButton(
+                
+                  onPressed:() {
+                  print("botton",);
+                }, child: Text("Login",style: TextStyle(
+                  fontSize: 25.0,
+                ),),
+                ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Signup"),
+                  Text("Forgot Password"),
+                ],
+              )
+
+
+              ],
+            ),
+          )
+        ]) ,
+
+      ),
+    );
+  }
+}
